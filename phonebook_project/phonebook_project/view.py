d84f853be200879d225cf2b23c64f412a3c8683e
Ritik raj verma <rajritik077@gmail.com>
2024-02-08 13:07:08 +0530

    changes

---
+++ phonebook_project/phonebook_project/view.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Contact
+from .forms import ContactForm
+
+def add_record(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_records')
+    else:
+        form = ContactForm()
+    return render(request, 'add_record.html', {'form': form})
+
+def list_records(request):
+    contacts = Contact.objects.all()
+    return render(request, 'list_records.html', {'contacts': contacts})
+
+def edit_record(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('list_records')
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'edit_record.html', {'form': form})
+
+def delete_record(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    contact.delete()
+    return redirect('list_records')
+
+def search_record(request):
+    query = request.GET.get('query')
+    if query:
+        contacts = Contact.objects.filter(name__icontains=query)
+    else:
+        contacts = Contact.objects.all()
+    return render(request, 'search_record.html', {'contacts': contacts})
